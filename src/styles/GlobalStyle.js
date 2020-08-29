2c106897c577e5a9b5caf4b88998e1249d33ec9f
Florian Soller <flsoller@gmail.com>
2020-08-29 18:29:51 +0200

    Add SVG icon styles.

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,12 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
- * {
-     padding: 0;
-     margin: 0;
-     box-sizing: border-box;
-     font-family: 'Roboto', sans-serif;
- }
- `;
+    html {
+            font-size: 16px;
+            box-sizing: border-box;
+        }
+
+    *, *:before, *:after {
+            padding: 0;
+            margin: 0;
+            box-sizing: inherit;
+        }
+
+    body {
+		    font-size: 1rem;
+		    font-family: 'Roboto', sans-serif;
+            color: #F3F3F3;
+            background-color: #1C252C;
+    }
+    `;
 
 export default GlobalStyle;
