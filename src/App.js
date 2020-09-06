0183e2f8a7aec5e8c765cbf9095c4775ae52f9f8
Florian Soller <flsoller@gmail.com>
2020-09-06 19:13:16 +0200

    Add celsius conversion.

--- src/App.js
+++ src/App.js
@@ -7,17 +7,23 @@ import ActiveLocation from './components/ActiveLocation';
 import CurrentCondition from './components/CurrentCondition';
 import ForecastContainer from './components/Forecast/ForecastContainer';
 
+const celsiusConversion = (value) => value - 273.15;
+
 function App() {
   const [Weather, setWeather] = useState({ city: '', condition: '', temp: '' });
+  //const [forecast, setForecast] = useState();
 
+  // set state to API response for selected values.
   const handleWeatherState = (input) => {
     console.log(input);
     setWeather({
       city: input.name,
-      condition: input.weather[0].main,
-      temp: input.main.temp,
+      condition: input.weather[0].main + ', ',
+      // Directly coverts API response from Kelvin to °C
+      temp: celsiusConversion(input.main.temp).toFixed(0) + ' °C',
     });
   };
+
   return (
     <AppContainer>
       <DataContainer>
