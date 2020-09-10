2ab10646707a7b7a3abf01cec2bfacd7eaed7dcc
Florian Soller <flsoller@gmail.com>
2020-09-10 13:35:04 +0200

    Updates

---
+++ precache-manifest.87de612407476d508ff1728bec9d5927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e1a6d01688ed218f3b8443f5ad403c",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "3364dee855a9c3f9b787",
+    "url": "/weather-app/static/js/2.8abfddb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.8abfddb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc08de9196f9165fd996",
+    "url": "/weather-app/static/js/main.472eb9f3.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "1653b81251f70836564618cb26c14780",
+    "url": "/weather-app/static/media/bg.1653b812.jpg"
+  }
+]);
